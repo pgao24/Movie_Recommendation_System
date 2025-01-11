080775677cf82c959f361442b03dcd0f8b2251c0
pgao24 <88458910+pgao24@users.noreply.github.com>
2025-01-10 20:54:49 -0800

    Adding server handler request to google in http_server.go (#4)
    Co-authored-by: Sally <pgao0624@gmail.cpm>

--- go_code/http_server.go
+++ go_code/http_server.go
@@ -3,15 +3,38 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
 )
+
+func root_handler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func relay_google(w http.ResponseWriter, r *http.Request ){
+ 	res, err := http.Get("https://google.com")
+ 	if err != nil {
+ 		fmt.Fprintf(w, "error making http request: %s\n", err)
+  		return
+	}
+        defer res.Body.Close()
+	if res.StatusCode == http.StatusOK {
+    		bodyBytes, err := io.ReadAll(res.Body)
+    		if err != nil {
+        		fmt.Fprintf(w, "error accessing response body:  %s\n", err)
+    			return
+		}
+    		bodyString := string(bodyBytes)
+    		fmt.Fprintf(w, bodyString)
+		return
+	} 
+	fmt.Fprintf(w, "error getting reponse status: %s\n", res.StatusCode)
+}
 
 func main() {
 	fmt.Println("Starting server")
 	// Handler function for the :root path ("/")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
-
+	http.HandleFunc("/", root_handler)
+        http.HandleFunc("/relay_google", relay_google)
 	// Start the server listening on port 8080
 	fmt.Println("Server listening on http://localhost:8080/")
 	http.ListenAndServe(":8080", nil)
